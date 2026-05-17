231edae320ff54cecae9f1d00deb559919446a98
Ethan Marx <ethanmarx2@gmail.com>
2026-05-17 03:24:27 -0700

    update workspaces

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -33,6 +33,7 @@ local fileManager = "dolphin"
 local menu        = "hyprlauncher"
 local browser     = "google-chrome-stable"
 local email		  = "betterbird"
+local music		  = "spotify-launcher"
 
 -------------------
 ---- AUTOSTART ----
@@ -280,7 +281,7 @@ local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + F", hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + M", hl.dsp.exec_cmd(fileManager))
+hl.bind(mainMod .. " + M", hl.dsp.exec_cmd(music))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + O", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(browser))
